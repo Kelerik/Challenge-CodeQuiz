330246042aa1b1995986f3aa34dbe777653439e8
Kelerik <vincent.ng39@gmail.com>
2021-11-06 19:43:16 -0400

    create variables

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+//  array of names and their corresponding scores
+var highscores = [];
+
+// current score/remaining time
+var score = 0;
+
+// all the quiz material
+var quiz = [
+    {
+        question: "What is JavaScript?",
+        // the FIRST item in the array is the correct answer
+        choices: [
+            "A programming language",
+            "An operating system",
+            "A super powerful AI",
+            "A writing system used by our ancient ancestors",
+        ],
+    },
+];
